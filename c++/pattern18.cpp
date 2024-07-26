5d794f589fa0994fb112240a54463e10d4e17034
anika-jahin <anikatasnim1228@gmail.com>
2024-07-27 02:05:24 +0600

    Star triangle reverse

---
+++ c++/pattern18.cpp
@@ -0,0 +1,25 @@
+/*Question: Print the pattern
+     *
+    **
+   ***
+  **** */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int row;
+    cout<<"Enter the rows: ";
+    cin>>row;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=row-i;j++)
+        {
+           cout<<"  ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+           cout<<"* ";
+        }
+        cout<<endl; 
+    }
+}
