5b8010ac60ea91b5a4591568f6c54453b9ac5800
anika-jahin <anikatasnim1228@gmail.com>
2024-08-02 19:07:35 +0600

    Delete java/printingNumber.java

--- java/printingNumber.java
+++
@@ -1,9 +0,0 @@
-public class printingNumber {
-    
-    public static void main(String[] args) {
-        System.out.println(8+2);
-        System.out.println(5);
-        System.out.println("4");
-        System.out.println("8+2");
-    }
-}
